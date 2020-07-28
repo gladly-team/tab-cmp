ce6b85831d1dba2bdfa379449c58428316bb7b44
Kevin Jennison <kevin.jennison1@gmail.com>
2020-07-28 10:55:47 -0700

    Add options to console log

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 export const getCMPHeadScript = () => {}
 
-export const initializeCMP = () => {
+export const initializeCMP = (options) => {
   // eslint-disable-next-line no-console
-  console.log('[tab-cmp] Called initializeCMP.')
+  console.log(
+    `[tab-cmp] Called initializeCMP with options: ${JSON.stringify(options)}`
+  )
 }
