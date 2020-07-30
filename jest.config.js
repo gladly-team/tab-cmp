829fd6f70a7e3b7a9f593e2783a5c3b6528f3786
Kevin Jennison <kevin.jennison1@gmail.com>
2020-07-30 16:10:43 -0700

    Use jest.config.js file and ignore modified QC CMP JS

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  collectCoverageFrom: [
+    'src/**',
+    '!**/__mocks__/**',
+    '!**/__tests__/**',
+    '!src/qcCmpModified.js',
+  ],
+  coverageDirectory: './coverage/',
+  setupFilesAfterEnv: ['<rootDir>/src/jestSetup.js'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/build/',
+    '/.yalc/',
+    '/.git/',
+    '/quantcast/',
+    '/src/qcCmpModified.js',
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '/.yalc/',
+    '^.+\\.module\\.(css|sass|scss)$',
+    '/quantcast/',
+    '/src/qcCmpModified.js',
+  ],
+}
