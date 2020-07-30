a004a7ce966a9ae2ad11badf2b36893ddef8eb88
Kevin Jennison <kevin.jennison1@gmail.com>
2020-07-30 12:01:26 -0700

    Add API stubs

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,37 @@
-
 describe('index.js', () => {
+  it('defines getCMPHeadScript', () => {
+    expect.assertions(1)
+    const index = require('src/index')
+    expect(index.getCMPHeadScript).toBeDefined()
+  })
+
   it('defines initializeCMP', () => {
     expect.assertions(1)
     const index = require('src/index')
     expect(index.initializeCMP).toBeDefined()
+  })
+
+  it('defines doesGDPRApply', () => {
+    expect.assertions(1)
+    const index = require('src/index')
+    expect(index.doesGDPRApply).toBeDefined()
+  })
+
+  it('defines doesCCPAApply', () => {
+    expect.assertions(1)
+    const index = require('src/index')
+    expect(index.doesCCPAApply).toBeDefined()
+  })
+
+  it('defines openTCFConsentDialog', () => {
+    expect.assertions(1)
+    const index = require('src/index')
+    expect(index.openTCFConsentDialog).toBeDefined()
+  })
+
+  it('defines openCCPAConsentDialog', () => {
+    expect.assertions(1)
+    const index = require('src/index')
+    expect(index.openCCPAConsentDialog).toBeDefined()
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-export const getCMPHeadScript = () => {}
+export const getCMPHeadScript = () => {
+  // eslint-disable-next-line no-console
+  console.log(`[tab-cmp] TODO: getCMPHeadScript`)
+}
 
 export const initializeCMP = (options) => {
   // eslint-disable-next-line no-console
   console.log(
     `[tab-cmp] Called initializeCMP with options: ${JSON.stringify(options)}`
   )
+}
+
+export const doesGDPRApply = async () => {
+  // eslint-disable-next-line no-console
+  console.log(`[tab-cmp] TODO: doesGDPRApply`)
+}
+
+export const doesCCPAApply = async () => {
+  // eslint-disable-next-line no-console
+  console.log(`[tab-cmp] TODO: doesCCPAApply`)
+}
+
+export const openTCFConsentDialog = async () => {
+  // eslint-disable-next-line no-console
+  console.log(`[tab-cmp] TODO: openTCFConsentDialog`)
+}
+
+export const openCCPAConsentDialog = async () => {
+  // eslint-disable-next-line no-console
+  console.log(`[tab-cmp] TODO: openCCPAConsentDialog`)
 }
