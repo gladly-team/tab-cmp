5d8c1ce87d2594aed0d0a4e59c3fc5a0d1150d8f
Kevin Jennison <kevin.jennison1@gmail.com>
2020-07-30 16:33:41 -0700

    Revert "Add testMatch to jest.config.js"
    This reverts commit 46ebc8c76d5c7a15bb20ecc1a9dbecc55328af90.

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
   coverageDirectory: './coverage/',
   setupFilesAfterEnv: ['<rootDir>/src/jestSetup.js'],
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/*.js'],
   testPathIgnorePatterns: [
     '/node_modules/',
     '/build/',
