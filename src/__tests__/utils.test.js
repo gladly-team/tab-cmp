6cdb647ff63633c442906a3e9de5730a9282f33b
Kevin Jennison <kevin.jennison1@gmail.com>
2020-08-10 12:35:10 -0700

    Add some tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,4 +1,13 @@
+import MockDate from 'mockdate'
+
+const mockNow = '2019-12-24T14:32:01.000Z'
+
+beforeEach(() => {
+  MockDate.set(mockNow)
+})
+
 afterEach(() => {
+  MockDate.reset()
   jest.clearAllMocks()
 })
 
@@ -31,6 +40,21 @@ describe('utils: isNil', () => {
     expect.assertions(1)
     const { isNil } = require('src/utils')
     expect(isNil(false)).toBe(false)
+  })
+})
+
+describe('utils: getCurrentISOTimestamp', () => {
+  test('returns the expected value', () => {
+    expect.assertions(1)
+    const { getCurrentISOTimestamp } = require('src/utils')
+    expect(getCurrentISOTimestamp()).toEqual('2019-12-24T14:32:01.000Z')
+  })
+
+  test('returns the expected value for another timestamp', () => {
+    expect.assertions(1)
+    MockDate.set('2020-03-10T00:00:01.000Z')
+    const { getCurrentISOTimestamp } = require('src/utils')
+    expect(getCurrentISOTimestamp()).toEqual('2020-03-10T00:00:01.000Z')
   })
 })
 
