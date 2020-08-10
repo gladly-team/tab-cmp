c1e7a64c9ccc6713bbc90ff28420172e86d945b7
Kevin Jennison <kevin.jennison1@gmail.com>
2020-08-10 14:03:23 -0700

    Add tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -12,31 +12,31 @@ afterEach(() => {
 })
 
 describe('utils: isNil', () => {
-  test('returns true for undefined', () => {
+  it('returns true for undefined', () => {
     expect.assertions(1)
     const { isNil } = require('src/utils')
     expect(isNil()).toBe(true)
   })
 
-  test('returns true for null', () => {
+  it('returns true for null', () => {
     expect.assertions(1)
     const { isNil } = require('src/utils')
     expect(isNil(null)).toBe(true)
   })
 
-  test('returns false for zero', () => {
+  it('returns false for zero', () => {
     expect.assertions(1)
     const { isNil } = require('src/utils')
     expect(isNil(0)).toBe(false)
   })
 
-  test('returns false for an empty string', () => {
+  it('returns false for an empty string', () => {
     expect.assertions(1)
     const { isNil } = require('src/utils')
     expect(isNil('')).toBe(false)
   })
 
-  test('returns false for false', () => {
+  it('returns false for false', () => {
     expect.assertions(1)
     const { isNil } = require('src/utils')
     expect(isNil(false)).toBe(false)
@@ -44,13 +44,13 @@ describe('utils: isNil', () => {
 })
 
 describe('utils: getCurrentISOString', () => {
-  test('returns the expected value', () => {
+  it('returns the expected value', () => {
     expect.assertions(1)
     const { getCurrentISOString } = require('src/utils')
     expect(getCurrentISOString()).toEqual('2019-12-24T14:32:01.000Z')
   })
 
-  test('returns the expected value for another timestamp', () => {
+  it('returns the expected value for another timestamp', () => {
     expect.assertions(1)
     MockDate.set('2020-03-10T00:00:01.000Z')
     const { getCurrentISOString } = require('src/utils')
@@ -59,13 +59,61 @@ describe('utils: getCurrentISOString', () => {
 })
 
 describe('utils: ISOStringToDate', () => {
-  test('works as expected', () => {
+  it('works as expected', () => {
     expect.assertions(1)
     const { ISOStringToDate } = require('src/utils')
     const ISOString = '2019-10-31T18:02:45.421Z' // UTC ms = 1572544965421
     const expectedDate = new Date(1572544965421)
     expect(ISOStringToDate(ISOString)).toEqual(expectedDate)
   })
+
+  it('throws if no input is provided', () => {
+    expect.assertions(1)
+    const { ISOStringToDate } = require('src/utils')
+    expect(() => {
+      ISOStringToDate()
+    }).toThrow()
+  })
+
+  it('throws if a number is provided', () => {
+    expect.assertions(1)
+    const { ISOStringToDate } = require('src/utils')
+    expect(() => {
+      ISOStringToDate(1572544965421)
+    }).toThrow()
+  })
 })
 
-// TODO: more tests
+describe('utils: getNumDaysBetweenDates', () => {
+  it('works for exactly 2 days', () => {
+    expect.assertions(1)
+    const { getNumDaysBetweenDates, ISOStringToDate } = require('src/utils')
+    const firstDate = ISOStringToDate('2019-10-29T18:02:45.421Z')
+    const laterDate = ISOStringToDate('2019-10-31T18:02:45.421Z')
+    expect(getNumDaysBetweenDates(laterDate, firstDate)).toEqual(2)
+  })
+
+  it('works for 2 days and change', () => {
+    expect.assertions(1)
+    const { getNumDaysBetweenDates, ISOStringToDate } = require('src/utils')
+    const firstDate = ISOStringToDate('2019-10-29T18:02:45.421Z')
+    const laterDate = ISOStringToDate('2019-10-31T19:04:12.001Z')
+    expect(getNumDaysBetweenDates(laterDate, firstDate)).toEqual(2.04266875)
+  })
+
+  it('returns a negative value when the inputs are reversed', () => {
+    expect.assertions(1)
+    const { getNumDaysBetweenDates, ISOStringToDate } = require('src/utils')
+    const firstDate = ISOStringToDate('2019-10-29T18:02:45.421Z')
+    const laterDate = ISOStringToDate('2019-10-31T19:04:12.001Z')
+    expect(getNumDaysBetweenDates(firstDate, laterDate)).toEqual(-2.04266875)
+  })
+
+  it('works for a multi-month time diff', () => {
+    expect.assertions(1)
+    const { getNumDaysBetweenDates, ISOStringToDate } = require('src/utils')
+    const firstDate = ISOStringToDate('2019-04-29T12:02:45.421Z')
+    const laterDate = ISOStringToDate('2019-10-31T18:02:45.421Z')
+    expect(getNumDaysBetweenDates(laterDate, firstDate)).toEqual(185.25)
+  })
+})
