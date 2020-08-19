08ba5c411bf950ebf76ee0ba8fdaa74e4e2b08c1
Kevin Jennison <kevin.jennison1@gmail.com>
2020-08-19 15:39:36 -0700

    Make __tcfapi init configurable

--- src/__tests__/initCMP.test.js
+++ src/__tests__/initCMP.test.js
@@ -1,8 +1,23 @@
 /* eslint no-underscore-dangle:0 */
+
+import { setUpQuantcastChoice } from 'src/qcChoiceModified'
+
+jest.mock('src/qcChoiceModified')
 jest.mock('src/qcCmpModified')
 
 beforeEach(() => {
   window.__tcfapi = jest.fn()
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+const getMockOptions = () => ({
+  publisherName: 'My Cool Site',
+  publisherLogo: 'https://example.com/some-logo.png',
+  displayPersistentConsentLink: true,
+  primaryButtonColor: '#FF0000',
 })
 
 describe('initCMP', () => {
@@ -10,5 +25,51 @@ describe('initCMP', () => {
     expect.assertions(1)
     const initCMP = require('src/initCMP').default
     expect(initCMP).toBeDefined()
+  })
+
+  it('calls setUpQuantcastChoice', () => {
+    expect.assertions(1)
+    const initCMP = require('src/initCMP').default
+    const opts = getMockOptions()
+    initCMP(opts)
+    expect(setUpQuantcastChoice).toHaveBeenCalled()
+  })
+
+  it('inits __tcfapi with the provided publisherName', () => {
+    expect.assertions(1)
+    const initCMP = require('src/initCMP').default
+    const opts = getMockOptions()
+    initCMP(opts)
+    const initData = window.__tcfapi.mock.calls[0][3]
+    expect(initData.coreConfig.publisherName).toEqual('My Cool Site')
+  })
+
+  it('inits __tcfapi with the provided publisherLogo', () => {
+    expect.assertions(1)
+    const initCMP = require('src/initCMP').default
+    const opts = getMockOptions()
+    initCMP(opts)
+    const initData = window.__tcfapi.mock.calls[0][3]
+    expect(initData.coreConfig.publisherLogo).toEqual(
+      'https://example.com/some-logo.png'
+    )
+  })
+
+  it('inits __tcfapi with the provided displayPersistentConsentLink value', () => {
+    expect.assertions(1)
+    const initCMP = require('src/initCMP').default
+    const opts = getMockOptions()
+    initCMP(opts)
+    const initData = window.__tcfapi.mock.calls[0][3]
+    expect(initData.coreConfig.displayPersistentConsentLink).toBe(true)
+  })
+
+  it('inits __tcfapi with the provided primaryButtonColor', () => {
+    expect.assertions(1)
+    const initCMP = require('src/initCMP').default
+    const opts = getMockOptions()
+    initCMP(opts)
+    const initData = window.__tcfapi.mock.calls[0][3]
+    expect(initData.theme.uxPrimaryButtonColor).toEqual('#FF0000')
   })
 })
