d5b3095f0f6811b710ea8ff3563275b23d3155a1
Kevin Jennison <kevin.jennison1@gmail.com>
2020-08-26 13:28:47 -0700

    Add tests

--- src/__tests__/updateStoredPrivacyData.test.js
+++ src/__tests__/updateStoredPrivacyData.test.js
@@ -1,6 +1,13 @@
-import { logError } from 'src/logger'
+import { logDebugging, logError } from 'src/logger'
+import localStorageMgr from 'src/localStorageMgr'
+import {
+  getMockUSPPingResponse,
+  getMockTCFDataInEU,
+  getMockTCFDataNonEU,
+} from 'src/test-utils'
 
 jest.mock('src/logger')
+jest.mock('src/localStorageMgr')
 
 beforeEach(() => {
   window.__tcfapi = jest.fn()
@@ -32,6 +39,90 @@ describe('updateStoredPrivacyData: TCF', () => {
       new Error(
         '[tab-cmp] Could not update TCF local storage data. window.__tcfapi is not defined.'
       )
+    )
+  })
+
+  it('sets the TCF local storage value when in the EU', () => {
+    expect.assertions(1)
+    const mockTCFData = getMockTCFDataInEU() // EU
+    window.__tcfapi.mockImplementation((cmd, version, callback) => {
+      switch (cmd) {
+        case 'getTCData': {
+          callback(mockTCFData, true)
+          break
+        }
+        default:
+      }
+    })
+    const updateStoredPrivacyData = require('src/updateStoredPrivacyData')
+      .default
+    updateStoredPrivacyData()
+    expect(localStorageMgr.setItem).toHaveBeenCalledWith(
+      'tabCMP.tcfv2.data',
+      JSON.stringify(mockTCFData)
+    )
+  })
+
+  it('sets the TCF local storage value when not in the EU', () => {
+    expect.assertions(1)
+    const mockTCFData = getMockTCFDataNonEU() // non-EU
+    window.__tcfapi.mockImplementation((cmd, version, callback) => {
+      switch (cmd) {
+        case 'getTCData': {
+          callback(mockTCFData, true)
+          break
+        }
+        default:
+      }
+    })
+    const updateStoredPrivacyData = require('src/updateStoredPrivacyData')
+      .default
+    updateStoredPrivacyData()
+    expect(localStorageMgr.setItem).toHaveBeenCalledWith(
+      'tabCMP.tcfv2.data',
+      JSON.stringify(mockTCFData)
+    )
+  })
+
+  it('calls logDebugging after updating local storage TCF data', () => {
+    expect.assertions(1)
+    const mockTCFData = getMockTCFDataInEU()
+    window.__tcfapi.mockImplementation((cmd, version, callback) => {
+      switch (cmd) {
+        case 'getTCData': {
+          callback(mockTCFData, true)
+          break
+        }
+        default:
+      }
+    })
+    const updateStoredPrivacyData = require('src/updateStoredPrivacyData')
+      .default
+    updateStoredPrivacyData()
+    expect(logDebugging).toHaveBeenCalledWith(
+      `Successfully updated TCF local storage data. Value:`,
+      mockTCFData
+    )
+  })
+
+  it('calls logDebugging when failing to update local storage TCF data', () => {
+    expect.assertions(1)
+    const mockTCFData = getMockTCFDataInEU()
+    window.__tcfapi.mockImplementation((cmd, version, callback) => {
+      switch (cmd) {
+        case 'getTCData': {
+          callback(mockTCFData, false) // false === unsuccessful
+          break
+        }
+        default:
+      }
+    })
+    const updateStoredPrivacyData = require('src/updateStoredPrivacyData')
+      .default
+    updateStoredPrivacyData()
+    expect(logDebugging).toHaveBeenCalledWith(
+      'Could not update TCF local storage data. The CMP errored and provided these data:',
+      mockTCFData
     )
   })
 })
