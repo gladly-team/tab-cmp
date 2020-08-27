5e072f587175a0da7e586e68f0f96d9587a3a6d4
Kevin Jennison <kevin.jennison1@gmail.com>
2020-08-27 14:30:20 -0700

    Add tests

--- src/__tests__/awaitCMPLoad.test.js
+++ src/__tests__/awaitCMPLoad.test.js
@@ -1,5 +1,5 @@
-// import { logDebugging } from 'src/logger'
-import { getMockTabCMPGlobal } from 'src/test-utils'
+import { logDebugging } from 'src/logger'
+import { getMockTabCMPGlobal, runAsyncTimerLoops } from 'src/test-utils'
 import awaitCMPLoad from 'src/awaitCMPLoad'
 
 jest.mock('src/logger')
@@ -21,7 +21,6 @@ afterEach(() => {
   jest.clearAllMocks()
 })
 
-// TODO: more tests
 describe('awaitCMPLoad', () => {
   it('resolves when the USP stub function is replaced', () => {
     expect.assertions(0)
@@ -44,5 +43,67 @@ describe('awaitCMPLoad', () => {
           throw e
         })
     })
+  })
+
+  it('rejects when the USP stub function is never replaced (it times out)', async () => {
+    expect.assertions(1)
+    const promise = awaitCMPLoad()
+      .then(() => {})
+      .catch((e) => {
+        expect(e).toEqual(new Error('Timed out while waiting for the CMP.'))
+      })
+
+    // Run out our timer that's used in polling the stub.
+    await runAsyncTimerLoops(40)
+
+    return promise
+  })
+
+  it('calls logDebugging as expected when the CMP responds successfully', async () => {
+    expect.assertions(2)
+    const promise = awaitCMPLoad()
+      .then(() => {
+        expect(logDebugging).toHaveBeenCalledWith(
+          `Polling for CMP to have loaded in updateStoredPrivacyData.`
+        )
+        expect(logDebugging).toHaveBeenCalledWith(
+          `Polling for CMP loaded. Loaded? true`
+        )
+      })
+      .catch((e) => {
+        throw e
+      })
+
+    // Mock that the CMP replaces the stub function.
+    window.__uspapi = () => {
+      // some new function
+    }
+
+    // Run out our timer that's used in polling the stub.
+    await runAsyncTimerLoops(40)
+
+    return promise
+  })
+
+  it('calls logDebugging as expected when the CMP fails to respond', async () => {
+    expect.assertions(3)
+    const promise = awaitCMPLoad()
+      .then(() => {})
+      .catch(() => {
+        expect(logDebugging).toHaveBeenCalledWith(
+          `Polling for CMP to have loaded in updateStoredPrivacyData.`
+        )
+        expect(logDebugging).toHaveBeenCalledWith(
+          `Polling for CMP loaded. Loaded? false`
+        )
+        expect(logDebugging).toHaveBeenCalledWith(
+          `Polling for CMP has timed out.`
+        )
+      })
+
+    // Run out our timer that's used in polling the stub.
+    await runAsyncTimerLoops(40)
+
+    return promise
   })
 })
